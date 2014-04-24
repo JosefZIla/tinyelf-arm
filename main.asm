c7f38670ec4144e0f10373f48af047f3a4f7872f
Josef Zila <josef.zila@acision.com>
2014-04-24 14:12:17 +0200

    cleanup

--- main.asm
+++ main.asm
@@ -11,25 +11,21 @@
                  bump       main                            @   e_version              @ p_filesz
                  bump       main                            @   e_entry                @ p_memsz
                  .word      4                               @   e_phoff                @ p_flags
-loop_count:      .word      3001                            @   e_shoff                @ p_allign
-rand_seed:       .word      0xfee1dead                      @   e_flags
-                 .word      0x200034                        @   e_ehsize & e_phentsize @ luckily valid instruction 
-                 svc        1                               @   e_phnum & e_shentsize
+loop_count:      .word      0x00000BB9                      @   e_shoff                @ p_allign
+rand_multiplyer: .word      0x41A7                          @   e_flags
+rand_seed:       .word      0x200034                        @   e_ehsize & e_phentsize
+                 .word      1                               @   e_phnum & e_shentsize
 symboltable:     .ascii     ".:;!]&@#"
-rand_multiplyer: .word      22695477
 main:            ldr        r4, rand_seed
                  ldr        r0, loop_count
-                 push       {r0}
-                 push       {r4}
-loop:            pop        {r4}
-                 pop        {r0}
+                 push       {r0,r4}
+loop:            pop        {r0,r4}
                  subs       r0, r0, #1
                  beq        end
-                 push       {r0}
                  ldr        r5, rand_multiplyer
                  mov        r3, #1
                  mla        r3, r4, r5, r3
-                 push       {r3}
+                 push       {r0,r3}
                  and        r3, r3, #458752
                  asr        r3, r3, #16
 print_char:      mov        r7, #4
