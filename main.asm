d5385aeafd2c3014520aa7d85b0118df8421847a
Josef Zila <josef.zila@acision.com>
2014-05-15 09:51:35 +0200

    minimal binary, added readelf target to makefile

--- main.asm
+++ main.asm
@@ -11,30 +11,8 @@
                  bump       main                            @   e_version              @ p_filesz
                  bump       main                            @   e_entry                @ p_memsz
                  .word      4                               @   e_phoff                @ p_flags
-loop_count:      .word      0x00000BB9                      @   e_shoff                @ p_allign
-rand_multiplier: .word      0x41A7                          @   e_flags
-rand_seed:       .word      0x200034                        @   e_ehsize & e_phentsize
-                 .word      1                               @   e_phnum & e_shentsize
-symboltable:     .ascii     ".:;!]&@#"
-main:            ldr        r4, rand_seed
-                 ldr        r0, loop_count
-                 push       {r0,r4}
-loop:            pop        {r0,r4}
-                 subs       r0, r0, #1
-                 beq        end
-                 ldr        r5, rand_multiplier
-                 mov        r3, #1
-                 mla        r3, r4, r5, r3
-                 push       {r0,r3}
-                 and        r3, r3, #0x70000
-                 asr        r3, r3, #0x10
-print_char:      mov        r7, #4
-                 mov        r0, #1
-                 adr        r1, symboltable
-                 add        r1, r1, r3
-                 mov        r2, #1
-                 svc        0
-                 b          loop
-end:             mov        r7, #1
-                 mov        r0, #42
+main:            mov        r7, #1                          @   e_shoff                @ p_allign
+                 mov        r0, #42                         @   e_flags
+                 .word      0x200034                        @   e_ehsize & e_phentsize
+                 .word      0x00000001                      @   e_phnum & e_shentsize
                  svc        0
